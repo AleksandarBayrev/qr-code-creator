b87b3f4575196948a6c1037a5a28819f5cefde16
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-10-23 19:14:50 +0300

    Update program

--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@
 using QRCodeCreator;
 using QRCoder;
 
-var config = JsonSerializer.Deserialize<AppConfig>(await File.ReadAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(), "config.json")));
-
+var config = JsonSerializer.Deserialize<AppConfig?>(await File.ReadAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(), "config.json")));
+if (config == null || config.Link == null || config.Link.Length == 0)
+{
+    await Console.Out.WriteLineAsync("Please update config.json with a link!");
+    return;
+}
+await Console.Out.WriteLineAsync($"Generating QR code for link: {config.Link}");
 using (QRCodeGenerator generator = new QRCodeGenerator())
 using (QRCodeData data = generator.CreateQrCode(config.Link, QRCodeGenerator.ECCLevel.Q))
 using (PngByteQRCode code = new PngByteQRCode(data))
