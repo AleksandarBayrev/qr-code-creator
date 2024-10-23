fa5cac9225d80bf572e386201af28ed9d03fe6db
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-10-23 19:59:12 +0300

    Update exception

--- ConfigInvalidException.cs
+++ ConfigInvalidException.cs
@@ -2,6 +2,11 @@ namespace QRCodeCreator
 {
     public class ConfigInvalidException : Exception
     {
-        public ConfigInvalidException(string filePath) : base($"JSON configuration is invalid for file: {filePath}") {}
-    }
+        public ConfigInvalidException(string filePath, string additionalMessage) : base($"JSON configuration is invalid for file: {filePath}")
+        {
+            AdditionalMessage = additionalMessage;
+        }
+
+		public string AdditionalMessage { get; }
+	}
 }
